90eff6f0c30a86ab3e56f4587de65d7fae2e0a7b
poornaka <99286090+IT21381218@users.noreply.github.com>
2024-12-19 16:08:14 +0530

    f

--- zade/src/App.js
+++ zade/src/App.js
@@ -1,6 +1,7 @@
 // import React, { useState, useEffect } from 'react';
-// import Header from './components/Header';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import Home from './components/Home';
+// import Projects from './components/Projects';
 // import Loading from './components/Loading';
 
 // function App() {
@@ -27,10 +28,12 @@
 //       {isLoading ? (
 //         <Loading />
 //       ) : (
-//         <div>
-//           <Header />
-//           <Home />
-//         </div>
+//         <Router>
+//           <Routes>
+//             <Route path="/" element={<Home />} />
+//             <Route path="/projects" element={<Projects />} />
+//           </Routes>
+//         </Router>
 //       )}
 //     </>
 //   );
